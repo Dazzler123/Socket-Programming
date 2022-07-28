78fc021e642d1b0162780a85fb3d4a8430aa3969
Dazzler123 <dasinduhewagamage@gmail.com>
2022-07-28 13:20:46 +0530

    Declaration of ServerSocket & LocalSocket

--- src/lk/ijse/socket_programming/ServerApp.java
+++ src/lk/ijse/socket_programming/ServerApp.java
@@ -1,4 +1,17 @@
 package lk.ijse.socket_programming;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        ServerSocket serverSocket = new ServerSocket(PORT);
+
+        System.out.println("Server is running in port : " + PORT);
+
+        Socket localSocket = serverSocket.accept();
+
+    }
 }
