adde21b606368b5c4e69e9a0d9cecb8e6e7b8d5b
Dazzler123 <dasinduhewagamage@gmail.com>
2022-07-29 23:52:57 +0530

    PrintWriter in ClientApp

--- src/lk/ijse/socket_programming/ClientApp.java
+++ src/lk/ijse/socket_programming/ClientApp.java
@@ -1,11 +1,16 @@
 package lk.ijse.socket_programming;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientApp {
     public static void main(String[] args) throws IOException {
         final int PORT = 8000;
         Socket socket = new Socket("localhost",PORT);
+
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("Hello there how are u ?");
+
     }
 }
