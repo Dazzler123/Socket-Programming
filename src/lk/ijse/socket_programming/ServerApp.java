246ff5b8f74bc9fd8a905eedaa13df5b0b84f926
Dazzler123 <dasinduhewagamage@gmail.com>
2022-07-30 01:21:06 +0530

    ServerApp updated

--- src/lk/ijse/socket_programming/ServerApp.java
+++ src/lk/ijse/socket_programming/ServerApp.java
@@ -5,23 +5,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         final int PORT = 8000;
-        ServerSocket serverSocket = new ServerSocket(PORT);
+        try {
+            //create server socket
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running in port : " + PORT);
 
-        System.out.println("Server is running in port : " + PORT);
+            //waiting for a client
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client Accepted");
 
-        Socket localSocket = serverSocket.accept();
-        System.out.println("Client Accepted");
+            DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
 
-        DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
 
-        DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String message = "", reply = "";
 
-        String message = "", reply = "";
+            while(!message.equals("finish")){
+                message = dataInputStream.readUTF();
+                System.out.println(message);
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
+
 }
