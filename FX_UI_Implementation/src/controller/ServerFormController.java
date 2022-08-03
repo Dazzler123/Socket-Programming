a785ea52634d5af40e496bdb6d45636d6d53b151
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-03 09:00:50 +0530

    Introduced FX UI implementation with server ui

---
+++ FX_UI_Implementation/src/controller/ServerFormController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ServerFormController {
+    public TextArea txtTextArea;
+    public TextField txtMessageHere;
+
+    public void btnSendMessage(ActionEvent actionEvent) {
+    }
+}
