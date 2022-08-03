8951330967c188a1bfd4143a9f5e757489da2b49
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-03 09:17:24 +0530

    Client ui implemented

--- FX_UI_Implementation/src/controller/ServerFormController.java
+++ FX_UI_Implementation/src/controller/ServerFormController.java
@@ -4,9 +4,37 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
     public TextArea txtTextArea;
     public TextField txtMessageHere;
+
+    final int PORT = 5000;
+    ServerSocket serverSocket;
+    Socket accept;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public void initialize(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(PORT);
+                    System.out.println("Server Started");
+                    accept = serverSocket.accept();
+                    System.out.println("Client Connected");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
 
     public void btnSendMessage(ActionEvent actionEvent) {
     }
