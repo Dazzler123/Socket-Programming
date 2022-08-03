b3db3400b494b16ac430bd83f592fe214457ca3f
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-03 09:05:02 +0530

    Implemented client ui

--- FX_UI_Implementation/src/controller/ClientFormController.java
+++ FX_UI_Implementation/src/controller/ClientFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ClientFormController {
+    public TextArea txtTextArea;
+    public TextField txtMessageHere;
+
+    public void btnSendMessage(ActionEvent actionEvent) {
+    }
 }
